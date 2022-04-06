85ec94c066e29d972dae22f9cb63e99f664440f9
Amy Pellegrini <amyvpellegrini@gmail.com>
2022-04-06 08:56:54 +0100

    Handle gun angle with arrows

--- src/client.js
+++ src/client.js
@@ -8,13 +8,14 @@ const height = terrain.height;
 const altitude = height / 2;
 
 const tankHeight = 12;
-const tankWidth = 48;
+const tankWidth = 36;
 const tankX = 200;
 const tankY = altitude - tankHeight + 1;
 
 const gunX = tankX + tankWidth / 2;
 const gunY = tankY;
 
+// draw terrain
 for (let x = 0; x < width * 4; x++) {
   for (let y = height; y > altitude; y--) {
     data[4 * (x + y * width) + 1] = 120;
@@ -22,9 +23,56 @@ for (let x = 0; x < width * 4; x++) {
   }
 }
 
+// draw tank body
 ctx.putImageData(terrain, 0, 0);
 ctx.fillRect(tankX, tankY, tankWidth, tankHeight);
-ctx.beginPath();
-ctx.moveTo(gunX, gunY);
-ctx.lineTo(gunX + 10, gunY - 10);
-ctx.stroke();
+
+let gunAngle = 45;
+
+function drawGun() {
+  ctx.beginPath();
+  ctx.strokeStyle = "black";
+
+  ctx.translate(gunX, gunY);
+
+  const radian = (gunAngle * Math.PI) / 180;
+
+  ctx.rotate(radian);
+  ctx.translate(-gunX, -gunY);
+
+  ctx.moveTo(gunX, gunY);
+  ctx.lineTo(gunX + 10, gunY - 10);
+  ctx.stroke();
+
+  ctx.closePath();
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+}
+
+drawGun();
+
+function eraseGun() {
+  ctx.clearRect(gunX - tankWidth / 2, gunY - 15, tankWidth, 15);
+}
+
+const handleLeftKeyPress = (event) => {
+  eraseGun();
+  gunAngle--;
+  drawGun();
+};
+
+const handleRightKeyPress = (event) => {
+  eraseGun();
+  gunAngle++;
+  drawGun();
+};
+
+const keyPressMap = {
+  37: handleLeftKeyPress,
+  39: handleRightKeyPress,
+};
+
+document.onkeydown = (event) => {
+  if (keyPressMap[event.keyCode]) {
+    keyPressMap[event.keyCode](event);
+  }
+};
